5da3613f7ede09b2ab802a4d6b0e832bb40e3c59
sambeet0809 <mandalsambeet94@gmail.com>
2026-07-25 13:30:52 +0530

    Add: boats-to-save-people - submission-2

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-2.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+
+        Arrays.sort(people);
+        int l = 0, r = people.length-1,count=0;
+
+        while(l<=r) {
+            if(people[r] == limit) {
+                count++; r--;
+            }
+            else if(people[l]+ people[r] <= limit) {
+                count++; l++; r--;
+            }
+            else{
+                count++;
+                r--;
+            }
+
+        }
+        //if(l==r) count+=1;
+        return count;
+        
+    }
+}
